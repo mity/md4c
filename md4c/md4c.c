30bf5667546cdd249227817534c30ca42c3c1032
Martin Mitas <mity@morous.org>
2019-03-27 02:04:49 +0200

    md_is_link_destination: Use first char as an indicator...
    ... type of link destination we should try.
    
    If it is '<', then try only <>-styled link destination.

--- md4c/md4c.c
+++ md4c/md4c.c
@@ -2023,8 +2023,10 @@ static inline int
 md_is_link_destination(MD_CTX* ctx, OFF beg, OFF max_end, OFF* p_end,
                        OFF* p_contents_beg, OFF* p_contents_end)
 {
-    return (md_is_link_destination_A(ctx, beg, max_end, p_end, p_contents_beg, p_contents_end)  ||
-            md_is_link_destination_B(ctx, beg, max_end, p_end, p_contents_beg, p_contents_end));
+    if(CH(beg) == _T('<'))
+        return md_is_link_destination_A(ctx, beg, max_end, p_end, p_contents_beg, p_contents_end);
+    else
+        return md_is_link_destination_B(ctx, beg, max_end, p_end, p_contents_beg, p_contents_end);
 }
 
 static int
