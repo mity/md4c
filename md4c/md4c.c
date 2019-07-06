43342b1149a0164057c13bdfd7a615be92e55a69
Tilman Roeder <dyed.green.info@gmail.com>
2019-07-06 12:50:07 +0100

    Addresses PR comments
    Specifically include correct OPENERS_CHAIN_LAST
    and no longer allows mixed openers/closers.

--- md4c/md4c.c
+++ md4c/md4c.c
@@ -141,7 +141,7 @@ struct MD_CTX_tag {
 #define BRACKET_OPENERS                         ctx->mark_chains[10]
 #define DOLLAR_OPENERS                          ctx->mark_chains[11]
 #define OPENERS_CHAIN_FIRST                     2
-#define OPENERS_CHAIN_LAST                      10
+#define OPENERS_CHAIN_LAST                      11
 
     int n_table_cell_boundaries;
 
@@ -3654,17 +3654,27 @@ md_analyze_dollar(MD_CTX* ctx, int mark_index)
 {
     /* This should mimic the way inline equations work in LaTeX, so there
      * can only ever be one item in the chain (i.e. the dollars can't be
-     * nested). This is basically the same as the md_analyze_tilde function.
+     * nested). This is basically the same as the md_analyze_tilde function,
+     * except that we require matching openers and closers to be of the same
+     * length.
      *
-     * Note: Doing things this way means we are very lenient and accept and
-     * number (1,2) of $ to close an equation. */
+     * E.g.: $abc$$def$$ => abc (display equation) def (end equation) */
     if(DOLLAR_OPENERS.head >= 0) {
-        /* The chain already contains an opener, so we are the closer. */
-        int opener_index = DOLLAR_OPENERS.head;
+        /* If the potential closer has a non-matching number of $, discard */
+        MD_MARK open = ctx->marks[DOLLAR_OPENERS.head];
+        MD_MARK close = ctx->marks[mark_index];
 
+        int opener_index = DOLLAR_OPENERS.head;
         md_rollback(ctx, opener_index, mark_index, MD_ROLLBACK_CROSSING);
-        md_resolve_range(ctx, &DOLLAR_OPENERS, opener_index, mark_index);
+        if (open.end - open.beg == close.end - close.beg) {
+            /* We are the matching closer */
+            md_resolve_range(ctx, &DOLLAR_OPENERS, opener_index, mark_index);
+        } else {
+            /* We don't match the opener, so discard old opener and insert as opener */
+            md_mark_chain_append(ctx, &DOLLAR_OPENERS, mark_index);
+        }
     } else {
+        /* No unmatched openers, so we are opener */
         md_mark_chain_append(ctx, &DOLLAR_OPENERS, mark_index);
     }
 }
