f4f899442c90dc905a67b05c29658ba7e779b5e5
Rasmus Andersson <rasmus@notion.se>
2020-10-18 10:36:37 -0700

    adds -clean option to scripts/build.sh and places debug & release builds in separate subdirectories under build

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,6 +9,7 @@ function usage {
   echo "  -debug       Build in debug mode instead of release mode"
   echo "  -lib=shared  Build dynamic shared library"
   echo "  -lib=static  Build static library"
+  echo "  -clean       Recreate build directory and build from scratch"
   echo "  -h, -help    Show help on stdout and exit"
   echo "Note on -lib:"
   echo "  If -lib is not provided, static library is built on Windows and"
@@ -18,6 +19,8 @@ function usage {
 }
 
 OPT_LIB=
+OPT_CLEAN=false
+BUILD_DIR=build/release
 CMAKE_ARGS=()
 export CMAKE_BUILD_TYPE=release
 export CFLAGS="-Wextra"
@@ -31,6 +34,11 @@ while [[ $# -gt 0 ]]; do
     ;;
   -debug|--debug)
     export CMAKE_BUILD_TYPE=debug
+    BUILD_DIR=build/debug
+    shift
+    ;;
+  -clean|--clean)
+    OPT_CLEAN=true
     shift
     ;;
   -lib=shared|--lib=shared)
@@ -55,27 +63,41 @@ if ! (which cmake >/dev/null); then
   exit 1
 fi
 
-CC=$CC
-if [ -z $CC ] && (which clang >/dev/null); then
-  export CC=clang
-fi
-if [ "$CC" == "clang" ] || [[ "$CC" == *"/clang" ]]; then
-  # enable colorized output (when stdout/stderr is a TTY) and warn about
-  # uninitialized variables that are initialized only in some conditions.
-  export CFLAGS="$CFLAGS -fcolor-diagnostics -Wconditional-uninitialized"
+if $OPT_CLEAN; then
+  rm -rf "$BUILD_DIR"
 fi
 
-rm -rf build
-mkdir build
-cd build
+SRCDIR=$PWD
 
-if (which ninja >/dev/null); then
-  cmake -G Ninja ..
-  ninja
-elif (which make >/dev/null); then
-  cmake -G "Unix Makefiles" ..
-  make
+if [ -d "$BUILD_DIR" ]; then
+  cd "$BUILD_DIR"
+  if [ -f build.ninja ]; then
+    ninja
+  else
+    make
+  fi
 else
-  echo "Could not find ninja nor make in PATH" >&2
-  exit 1
+  CC=$CC
+  if [ -z $CC ] && (which clang >/dev/null); then
+    export CC=clang
+  fi
+  if [ "$CC" == "clang" ] || [[ "$CC" == *"/clang" ]]; then
+    # enable colorized output (when stdout/stderr is a TTY) and warn about
+    # uninitialized variables that are initialized only in some conditions.
+    export CFLAGS="$CFLAGS -fcolor-diagnostics -Wconditional-uninitialized"
+  fi
+
+  mkdir -p "$BUILD_DIR"
+  cd "$BUILD_DIR"
+
+  if (which ninja >/dev/null); then
+    cmake -G Ninja "$SRCDIR"
+    ninja
+  elif (which make >/dev/null); then
+    cmake -G "Unix Makefiles" "$SRCDIR"
+    make
+  else
+    echo "Could not find ninja nor make in PATH" >&2
+    exit 1
+  fi
 fi
