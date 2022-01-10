mity/md4c

commit a8bb4d3020eb1cfa07f01241c2aa668d91011cb5

md_is_table_underline: Remove requirement for minimal length of a cell underline.
Fixes #169.

src/md4c.c
@@ -5212,19 +5212,17 @@ md_is_table_underline(MD_CTX* ctx, OFF beg, OFF* p_end, unsigned* p_col_count)
     }
 
     while(1) {
-        OFF cell_beg;
         int delimited = FALSE;
 
         /* Cell underline ("-----", ":----", "----:" or ":----:") */
-        cell_beg = off;
         if(off < ctx->size  &&  CH(off) == _T(':'))
             off++;
+        if(off < ctx->size  &&  CH(off) != _T('-'))
+            return FALSE;
         while(off < ctx->size  &&  CH(off) == _T('-'))
             off++;
         if(off < ctx->size  &&  CH(off) == _T(':'))
             off++;
-        if(off - cell_beg < 3)
-            return FALSE;
 
         col_count++;
 

commit c058e82c6a939f591059672dab08ddd0d4ce416e

md_is_table_underline: Fix detection by the end of file.
This was a regression in a8bb4d3020eb1cfa07f01241c2aa668d91011cb5.

src/md4c.c
@@ -5250,7 +5250,7 @@ md_is_table_underline(MD_CTX* ctx, OFF beg, OFF* p_end, unsigned* p_col_count)
         /* Cell underline ("-----", ":----", "----:" or ":----:") */
         if(off < ctx->size  &&  CH(off) == _T(':'))
             off++;
-        if(off < ctx->size  &&  CH(off) != _T('-'))
+        if(off >= ctx->size  ||  CH(off) != _T('-'))
             return FALSE;
         while(off < ctx->size  &&  CH(off) == _T('-'))
             off++;