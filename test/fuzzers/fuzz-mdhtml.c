a27f8dc0935e14c26f20b0875802b8ab3891349b
Martin Mitas <mity@morous.org>
2023-12-12 19:31:30 +0100

    test/fuzzers.fuzz-mdhtml.c: Remove stale comment.

--- test/fuzzers/fuzz-mdhtml.c
+++ test/fuzzers/fuzz-mdhtml.c
@@ -17,19 +17,15 @@ LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     unsigned parser_flags, renderer_flags;
 
+    /* We interpret the 1st 8 bytes as parser flags and renderer flags. */
     if(size < 2 * sizeof(unsigned)) {
-        /* We interpret the 1st 8 bytes as parser flags and renderer flags. */
         return 0;
     }
+    parser_flags = ((unsigned*)data)[0];
+    renderer_flags = ((unsigned*)data)[1];
+    data += 2 * sizeof(unsigned);
+    ize -= 2 * sizeof(unsigned);
 
-    parser_flags = *(unsigned*)data;
-    data += sizeof(unsigned); size -= sizeof(unsigned);
-
-    renderer_flags = *(unsigned*)data;
-    data += sizeof(unsigned); size -= sizeof(unsigned);
-
-    /* Allocate enough space */
     md_html(data, size, process_output, NULL, parser_flags, renderer_flags);
-
     return 0;
 }
