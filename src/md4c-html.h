3e93dd5e06fe47ce452df387c5948c0384fcbe43
Martin Mitáš <mity@morous.org>
2024-02-25 15:39:28 +0100

    Implement customizable HTML renderer.

--- src/md4c-html.h
+++ src/md4c-html.h
@@ -40,10 +40,13 @@
 #define MD_HTML_FLAG_XHTML                  0x0008
 
 
-/* Render Markdown into HTML.
+/* Simple do-it-all function for converting Markdown to HTML.
  *
  * Note only contents of <body> tag is generated. Caller must generate
  * HTML header/footer manually before/after calling md_html().
+ *
+ * For more control over the conversion (e.g. to customize the output), you may
+ * use more fine-grained API below.
  *
  * Params input and input_size specify the Markdown input.
  * Callback process_output() gets called with chunks of HTML output.
@@ -59,6 +62,80 @@
 int md_html(const MD_CHAR* input, MD_SIZE input_size,
             void (*process_output)(const MD_CHAR*, MD_SIZE, void*),
             void* userdata, unsigned parser_flags, unsigned renderer_flags);
+
+
+/* The functions below provide more finer-grained building blocks, which allow
+ * application to e.g. customize how (some) Markdown syntax constructions are
+ * converted into HTML.
+ *
+ * The call to md_html() above is morally equivalent to this code:
+ *
+ * ``` C
+ * #include "md4c.h"
+ * #include "md4c-html.h"
+ *
+ * int
+ * md_html(const MD_CHAR* input, MD_SIZE input_size,
+ *         void (*process_output)(const MD_CHAR*, MD_SIZE, void*),
+ *         void* userdata, unsigned parser_flags, unsigned renderer_flags)
+ * {
+ *     MD_HTML* mh;
+ *     MD_PARSER_v2 p;
+ *     int ret;
+ *
+ *     mh = md_html_create(process_output, userdata, parser_flags, renderer_flags);
+ *     if(mh == NULL)
+ *         return -1;
+ *
+ *     memset(&p, 0, sizeof(p));
+ *     p.abi_version = 2;
+ *     p.flags = parser_flags;
+ *     p.enter_block = md_html_enter_block;
+ *     p.leave_block = md_html_leave_block;
+ *     p.enter_span = md_html_enter_span;
+ *     p.leave_span = md_html_leave_span;
+ *     p.text = md_html_text;
+ *     p.debug_log = md_html_debug_log;
+ *
+ *     ret = md_parse(input, input_size, (MD_PARSER*) &p, (void*) mh);
+ *
+ *     md_html_destroy(mh);
+ *     return ret;
+ * }
+ * ```
+ *
+ * This allows application to implement its own callbacks for md_parse()
+ * which may provide custom output e.g. for some block and/or span types, and
+ * calls the original callback for block/span types it does not want to
+ * customize.
+ */
+
+/* An opaque structure representing the Markdown-to-HTML converter. */
+typedef struct MD_HTML MD_HTML;
+
+/* Create/destroy the Markdown-to-HTML converter structure. */
+MD_HTML* md_html_create(void (*process_output)(const MD_CHAR*, MD_SIZE, void*),
+                void* userdata, unsigned renderer_flags);
+void md_html_destroy(MD_HTML* mh);
+
+/* Standard HTML callbacks for MD_PARSER.
+ *
+ * (Application can use its own callback and use these functions as "fallback"
+ * for stuff it does not want to customize. In such case the application is
+ * responsible for propagating MD_HTML* returned from md_html_create() as
+ * userdata to these standard callbacks.)
+ */
+int md_html_enter_block(int block_type, void* detail, void* userdata);
+int md_html_leave_block(int block_type, void* detail, void* userdata);
+int md_html_enter_span(int span_type, void* detail, void* userdata);
+int md_html_leave_span(int span_type, void* detail, void* userdata);
+int md_html_text(int text_type, const MD_CHAR* text, MD_SIZE size, void* userdata);
+void md_html_debug_log(const char* msg, void* userdata);
+
+/* Functions to call from custom md_parser() callbacks, to make an output. */
+void md_html_output_verbatim(MD_HTML* mh, const MD_CHAR* test, MD_SIZE size);
+void md_html_output_escaped(MD_HTML* mh, const MD_CHAR* test, MD_SIZE size);
+void md_html_output_url_escaped(MD_HTML* mh, const MD_CHAR* test, MD_SIZE size);
 
 
 #ifdef __cplusplus
