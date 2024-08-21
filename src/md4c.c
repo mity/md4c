bd6a4e39d2a023699b160377e17f151262f91ab7
Makoto Emura <makoto@emurasoft.com>
2024-08-21 14:51:20 -0700

    Fix md_decode_utf16le_before__() return value

--- src/md4c.c
+++ src/md4c.c
@@ -855,7 +855,7 @@ struct MD_UNICODE_FOLD_INFO_tag {
         if(off > 2 && IS_UTF16_SURROGATE_HI(CH(off-2)) && IS_UTF16_SURROGATE_LO(CH(off-1)))
             return UTF16_DECODE_SURROGATE(CH(off-2), CH(off-1));
 
-        return CH(off);
+        return CH(off-1);
     }
 
     /* No whitespace uses surrogates, so no decoding needed here. */
