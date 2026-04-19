mity/md4c

commit fab28e4149591d9c98d45ded41f4d82efcac1a7a

Avoid repeated language- in info string.

src/md4c-html.c
@@ -301,7 +301,10 @@ render_open_code_block(MD_HTML* r, const MD_BLOCK_CODE_DETAIL* det)
 
     /* If known, output the HTML 5 attribute class="language-LANGNAME". */
     if(det->lang.text != NULL) {
-        RENDER_VERBATIM(r, " class=\"language-");
+        RENDER_VERBATIM(r, " class=\"");
+        if(strncmp(det->lang.text, "language-", 9) != 0) {
+            RENDER_VERBATIM(r, "language-");
+        }
         render_attribute(r, &det->lang, render_html_escaped);
         RENDER_VERBATIM(r, "\"");
     }

commit f24870b973b803ac9408877ded880724521ca310

render_open_code_block: Fix buffer overflow.
strncmp() could can touch a memory it should not, if the language
attribute is too short, possibly leading to buffer overflow if the
opening fence is very close to the document end.

The bug was introduced recently in the commit
fab28e4149591d9c98d45ded41f4d82efcac1a7a.

Fixes #305.

src/md4c-html.c
@@ -305,7 +305,7 @@ render_open_code_block(MD_HTML* r, const MD_BLOCK_CODE_DETAIL* det)
     /* If known, output the HTML 5 attribute class="language-LANGNAME". */
     if(det->lang.text != NULL) {
         RENDER_VERBATIM(r, " class=\"");
-        if(strncmp(det->lang.text, "language-", 9) != 0) {
+        if(det->lang.size < 9  ||  strncmp(det->lang.text, "language-", 9) != 0) {
             RENDER_VERBATIM(r, "language-");
         }
         render_attribute(r, &det->lang, render_html_escaped);