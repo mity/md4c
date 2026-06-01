bd01a73e36f2bbfe65431663e63e959edc0e2384
Zach Newton <znewt99@users.noreply.github.com>
2026-05-31 20:24:05 -0600

    Make tests more like cmark

--- test/prog.py
+++ test/prog.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from subprocess import *
+from subprocess import Popen, PIPE
 import platform
 import os
 
-def pipe_through_prog(argv, text):
-    p1 = Popen(argv, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+def pipe_through_prog(prog, text):
+    p1 = Popen(prog, stdout=PIPE, stdin=PIPE, stderr=PIPE)
     [result, err] = p1.communicate(input=text.encode('utf-8'))
     return [p1.returncode, result.decode('utf-8'), err]
 
 class Prog:
-    def __init__(self, cmdline="md2html", default_options=[]):
-        self.cmdline = cmdline.split()
-        if len(self.cmdline) <= 1:
-            # cmdline provided no command line options. Use default ones.
+    def __init__(self, prog="md2html", default_options=[]):
+        self.prog = prog.split()
+        if len(self.prog) <= 1:
+            # prog provided no command line options. Use default ones.
             if isinstance(default_options, str):
-                self.cmdline += default_options.split()
+                self.prog += default_options.split()
             else:
-                self.cmdline += default_options
-        self.to_html = lambda x: pipe_through_prog(self.cmdline, x)
+                self.prog += default_options
+        self.to_html = lambda x: pipe_through_prog(self.prog, x)
